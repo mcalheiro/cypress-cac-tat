7f83038b878e556dded1b1a46181e94abc1757d2
Mauricio <mauriciocalheiro95@gmail.com>
2025-07-21 17:10:06 +0100

    Create first test

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -1,5 +1,6 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('CAC TAT', () => {
+  it('Verify page title', () => {
+    cy.visit('/')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
   })
 })
