fe2b12575b4cb6769300fd41929ce15272e94ffb
Mauricio <mauriciocalheiro95@gmail.com>
2025-07-28 17:16:02 +0100

    Add video support

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000",
+  },
   viewportWidth: 1920,
   viewportHeight: 1080,
-  baseUrl: "http://localhost:3000",
-  },
+  video: true,
 });
